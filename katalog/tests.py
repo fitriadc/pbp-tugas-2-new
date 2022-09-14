f44dbc92365c065558f822c01479031760673a84
fitriadc <fitriadwicahya@gmail.com>
2022-09-14 14:52:41 +0700

    update code

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from katalog.models import CatalogItem
 
-# Create your tests here.
+
+class KatalogTestCase(TestCase):
+    def setUp(self):
+        CatalogItem.objects.create(item_name="cap", item_price=700, item_stock=4,
+                                   description="new item", rating=9, item_url="google.com")
+
+    def test_katalog_props_should_be_same(self):
+        """Animals that can speak are correctly identified"""
+        cap = CatalogItem.objects.get(item_name="cap", item_price=700, item_stock=4,
+                                      description="new item", rating=9, item_url="google.com")
+        self.assertEqual(cap.item_name, "cap")
+        self.assertEqual(cap.item_price, 700)
+        self.assertEqual(cap.item_stock, 4)
+        self.assertEqual(cap.description, "new item")
+        self.assertEqual(cap.rating, 9)
+        self.assertEqual(cap. item_url, "google.com")
